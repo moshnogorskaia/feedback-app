2c9da2607c89412e833666b1e85a34a9784f5f69
Marina Moshnogorskaia <mmoshnogorskaia@gmail.com>
2026-01-03 10:25:56 +0100

    Dynamic API route

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,35 @@
 import { buildFeedbackPath, extractFeedback } from '../api/feedback';
+import { useState } from 'react';
 
 function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
+
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && (
+        <p>
+          {feedbackData.email}: {feedbackData.text}
+        </p>
+      )}
+      <ul>
+        {props.feedbackItems.map(({ id, text }) => (
+          <li key={id}>
+            {text}{' '}
+            <button onClick={loadFeedbackHandler.bind(null, id)}>
+              Show Details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
